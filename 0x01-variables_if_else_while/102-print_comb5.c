d5cca50c519d11ae4624347b5daf4d5d230eb5d1
Ugo-simba <hugosimba49@gmail.com>
2022-03-13 14:22:37 -0700

    C script 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,7 +14,7 @@ int main(void)
 		a = i / 10; /* doubles fnum */
 		b = i % 10; /* singles fnum */
 
-		for (j + 0; j < 100; j++)
+		for (j = 0; j < 100; j++)
 		{
 			c = j / 10; /* doubles snum */
 			d = j % 10; /* singles smum */
