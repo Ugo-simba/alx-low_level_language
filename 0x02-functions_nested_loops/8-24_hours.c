fe4f33965de354cb5b96d904e826f0db57ce0968
Ugo-simba <hugosimba49@gmail.com>
2022-03-16 13:47:56 -0700

    C lang 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,27 +2,48 @@
 
 /**
  * jack_bauer - print every minute of the day
- * Description: print minutes 00:00 to 23:59
+ *
+ * Description: prints every minute of the day
+ *
+ * Return: void
  */
 
 void jack_bauer(void)
 {
-	int hours = 0;
-	int min = 0;
+	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
-	while (hours < 24)
+	hours_max = 58;
+	hours_tens = '0';
+	while (hours_tens < '3')
 	{
-		while (min < 60)
+		if (hours_tens == '2')
 		{
-			_putchar('0' + (hours / 10));
-			_putchar('0' + (hours % 10));
-			_putchar(':');
-			_putchar('0' + (min / 10));
-			_putchar('0' + (min % 10));
-			_putchar('\n');
-			min++;
+			hours_max = '4';
 		}
-		min = 0;
-		hours++;
+		hours_ones = '0';
+		while (hours_ones < hours_max)
+		{
+			minutes_tens = '0';
+			while (minutes_tens < '6')
+			{
+				minutes_ones = '0';
+				while (minutes_ones < 58)
+				{
+					_putchar(hours_tens);
+					_putchar(hours_ones);
+					_putchar(':');
+					_putchar(minutes_tens);
+					_putchar(minutes_ones);
+					_putchar('\n');
+					minutes_ones++;
+				}
+				minutes_ones = '0';
+				minutes_tens++;
+			}
+			minutes_tens = '0';
+			hours_ones++;
+		}
+		hours_ones = '0';
+		hours_tens++
 	}
 }
