88ef85cb7661d2bd364540748cb067d6bab76a9c
Ugo-simba <hugosimba49@gmail.com>
2022-03-24 17:15:25 -0700

    C 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,7 +2,7 @@
 /**
  * leet - encodes a string into 1337.
  * letters a and A are replaced by 4.
- * letters e and E are replaced by 3.
+ * Letters e and E are replaced by 3.
  * Letters o and O are replaced by 0.
  * Letters t and T are replaced by 7.
  * Letters l and L are replaced by 1.
@@ -24,7 +24,7 @@ char *leet(char *s)
 		leetCount = 0;
 		while (leetCount < 10)
 		{
-			if (leetLetter[leetCount] == s[stringCount])
+			if (leetLetters[leetCount] == s[stringCount])
 			{
 				s[stringCount] = leetNums[leetCount];
 			}
